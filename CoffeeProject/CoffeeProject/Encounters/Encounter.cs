a1b6f0aeb3b7dc51452f7506dd6d681d6e66ece3
draqqqula <121135567+draqqqula@users.noreply.github.com>
2023-11-01 00:23:38 +0500

    Созданы классы Room и Encounter

---
+++ CoffeeProject/CoffeeProject/Encounters/Encounter.cs
@@ -0,0 +1,25 @@
+﻿using CoffeeProject.GameObjects;
+using CoffeeProject.Layers;
+using MagicDustLibrary.Logic;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeProject.Encounters
+{
+    public abstract class Encounter
+    {
+        public abstract void Create(IStateController state, Point position);
+    }
+
+    public class EnemyEncounter : Encounter
+    {
+        public override void Create(IStateController state, Point position)
+        {
+            state.CreateObject<TestType, MainLayer>(position.ToVector2());
+        }
+    }
+}
---
+++ CoffeeProject/CoffeeProject/Room/Room.cs
@@ -0,0 +1,23 @@
+﻿using CoffeeProject.Encounters;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeProject.Room
+{
+    public class Room
+    {
+        public Rectangle Bounds { get; private set; }
+        public bool Completed { get; private set; }
+        public Encounter[] Encounters { get; private set; }
+
+        public Room(Rectangle bounds, Encounter[] encounters)
+        {
+            Bounds = bounds;
+            Encounters = encounters;
+        }
+    }
+}
