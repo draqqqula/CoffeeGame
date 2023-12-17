e6afee213ab562a408604a752664a6deaa17c9e9
draqqqula <121135567+draqqqula@users.noreply.github.com>
2023-12-17 18:57:30 +0500

    auto changes

--- CoffeeProject/JsonPipeline/obj/Debug/net6.0/JsonPipeline.AssemblyInfo.cs
+++ CoffeeProject/JsonPipeline/obj/Debug/net6.0/JsonPipeline.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JsonPipeline")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5177417a39f387e9fedcdc4b4b779c095c1de442")]
 [assembly: System.Reflection.AssemblyProductAttribute("JsonPipeline")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JsonPipeline")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
