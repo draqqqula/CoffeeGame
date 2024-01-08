4abe473496078fcf5cc7a3e8a3f9fcfdb6485338
draqqqula <121135567+draqqqula@users.noreply.github.com>
2024-01-09 03:23:31 +0500

    изменён профиль снова

--- CoffeeProject/CoffeeProject/obj/Debug/net8.0/CoffeeProject.AssemblyInfo.cs
+++ CoffeeProject/CoffeeProject/obj/Debug/net8.0/CoffeeProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoffeeProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7bf51d323d91c622f4a1368635078d28b252547")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoffeeProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoffeeProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
